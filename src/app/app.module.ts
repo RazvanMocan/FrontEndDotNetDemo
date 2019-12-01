59f1cdd0e1929490fba9616932a2e4218f731a4e
Razvan Mocan <razvanmocan15@gmail.com>
2019-12-01 10:26:52 +0200

    Get data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { TableComponent } from './table/table.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {MatPaginatorModule, MatTableModule} from '@angular/material';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +22,10 @@ import {MatPaginatorModule, MatTableModule} from '@angular/material';
       {path: 'table', component: TableComponent}
     ]),
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
